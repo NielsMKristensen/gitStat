67863592e4df82205b4c0f286754e57dd20289d8
ujjtamas <83408501+ujjtamas@users.noreply.github.com>
2022-02-05 20:08:48 +0100

    Update config.js
    Added config file for path code mapping

---
+++ config/config.js
@@ -0,0 +1,10 @@
+/*0 No config
+1 Commit statistics
+2 Page views
+4 Clones and referrals*/
+const codePathMap = {
+0:'',
+1:['/stats/code_frequency','/stats/participation','/stats/punch_card'],
+2:['/traffic/views'],
+4:['/traffic/clones','/traffic/popular/paths','/traffic/popular/referrers']
+}
