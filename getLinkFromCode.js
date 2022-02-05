b2669a5f1d5cdfea21c4ee06faa2061fa5a23b00
Tamás Ujj <Tamas.Ujj@LEGO.com>
2022-02-05 23:50:55 +0100

    Fixed decodeCode

--- getLinkFromCode.js
+++ getLinkFromCode.js
@@ -1,24 +1,28 @@
 //Decode the code to know what config has been requested
 function decodeCode(x,arr){
 let number = 0;
-if(x===0) return arr;
+let arr2 = arguments[1] == undefined ? [] : [...arr];
+
+if(x===0) {
+  return arr;
+}
 
 for(i=0;;i++){
-  if(x > Math.pow(2,i)) continue;
+  if(x >= Math.pow(2,i)) continue;
   number = Math.pow(2,i-1);
-  arr.push(number);
+  arr2.push(number);
   break;
 }
 x-=number;
-decodeCode(x,arr);
+return decodeCode(x,arr2);
 }
 
-//Fetch the path based on the config
-function getPath(x){
-  const arrayOfCodes= decodeCode(x);
-  const arrayOfPath = [];
-  for(i in arrayOfCodes){
-    //store and then get the link from somewhere
-    arrayOfPath.push();
-  }
-}
+Fetch the path based on the config
+ function getPath(x){
+   const arrayOfCodes= decodeCode(x);
+   const arrayOfPath = [];
+   for(i in arrayOfCodes){
+     //store and then get the link from somewhere
+     arrayOfPath.push();
+   }
+ }
