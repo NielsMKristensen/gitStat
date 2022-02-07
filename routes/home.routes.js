55c4d2fe5a40bb74fd535ae9603dc46aefc51da0
NielsMKristensen <Niels.Kristensen@Hotmail.com>
2022-02-07 10:22:02 +0100

    stuff

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -6,6 +6,6 @@ const User = require('../models/User.models');
 const { isLoggedIn, isLoggedOut } = require('../middlewares/middleware');
 
 //get login page. (should be start page. page home in our nice picture)
-router.get('/home', isLoggedIn, (req,res,next) => res.render('home.hbs'));
+router.get('/home', /*isLoggedIn,*/ (req,res,next) => res.render('home.hbs'));
 
 module.exports = router;
--- routes/register.routes.js
+++ routes/register.routes.js
@@ -5,7 +5,35 @@ const saltRounds = 10;
 const User = require('../models/User.models');
 const { isLoggedIn, isLoggedOut } = require('../middlewares/middleware');
 
-//get registration page. (not in our wirefraime description lets add)
+//GET registration page. (not in our wirefraime description lets add)
 router.get('/register', /*isLoggedOut*/ (req,res,next) => res.render('register.hbs'));
+
+//POST registration with info from registration page.
+router.post('/register', (req, res, next) => {
+    console.log('console stuff here',req.body)
+    
+    const { username, password, gitusernames } = req.body;
+
+    if (!username || !password || !gitusernames) {
+      res.render('/register', {errorMessage: 'All fields needs to be filled out, please provide username, password and GIT username'})  
+    }
+    bcryptjs
+      .genSalt(saltRounds)
+      .then(salt => bcryptjs.hash(password, salt))
+      .then(hashedPassword => {
+        return User.create({
+            username,
+            password: hashedPassword,
+            gitusernames
+        });
+      })
+      .then(userFromDB => {
+        res.redirect('/home');
+    })
+    .catch(error => {
+      next(error);
+    }) 
+});
+
 
 module.exports = router;
